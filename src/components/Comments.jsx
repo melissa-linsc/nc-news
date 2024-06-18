23013be879bf885cb3362a7030188fb67726461a
melissa-linsc <118235468+melissa-linsc@users.noreply.github.com>
2024-06-18 11:47:23 +0100

    Task 9 - delete comments

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,12 +1,15 @@
-import { getArticleComments } from "../utils/api";
-import { useState, useEffect } from "react";
+import { getArticleComments, deleteComments } from "../utils/api";
+import { useState, useEffect, useContext } from "react";
+import { UserContext } from "./UserProvider";
 import { useParams } from "react-router-dom";
 import { NewComment } from "./NewComment";
+import DeleteIcon from '@mui/icons-material/Delete';
 import '../styles/ArticlesById.css'
 
-export function Comments({setCommentCount, commentCount}) {
+export function Comments({setCommentCount, commentCount, setAlertMessage, setShowAlertMessage}) {
 
     const {article_id} = useParams()
+    const { currentUser } = useContext(UserContext)
 
     const [comments, setComments] = useState([])
     const [isLoading, setIsLoading] = useState(true)
@@ -26,6 +29,27 @@ export function Comments({setCommentCount, commentCount}) {
         return <p>No comments yet...</p>
     }
 
+    function handleDelete(commentToRemove) {
+       deleteComments(commentToRemove.comment_id).then(() => {
+        setAlertMessage('Comment Deleted!')
+       })
+       .catch((err)=> {
+        setAlertMessage('Error deleting comment, try again later')
+       })
+       setComments((currComments) => {
+        const filteredComments = currComments.filter((comment) => {
+            return comment.comment_id !== commentToRemove.comment_id
+        })
+        return filteredComments
+        })
+        const updatedCommentCount = commentCount - 1
+        setCommentCount(updatedCommentCount)
+        setShowAlertMessage(true)
+        setTimeout(() => {
+            setShowAlertMessage(false)
+        }, 4000)
+    }
+
     return (
         <>
             <NewComment comments={comments} setComments={setComments} setCommentCount={setCommentCount} commentCount={commentCount}/>
@@ -37,16 +61,19 @@ export function Comments({setCommentCount, commentCount}) {
 
                     const dateTime = date + " " + time
 
-                    return <li key={comment.comment_id}>
-                        <p id="comment-body">{comment.body}</p>
-                        <div id="comment-info">
-                            <div>
-                                <p id="comment-author">{comment.author}</p>
-                                <p id="comment-date">{dateTime}</p>
+                    return (
+                        <li key={comment.comment_id} >
+                            <p id="comment-body">{comment.body}</p>
+                            <div id="comment-info">
+                                <div>
+                                    <p id="comment-author">{comment.author}</p>
+                                    <p id="comment-date">{dateTime}</p>
+                                </div>
+                                <p id="comment-votes">votes: {comment.votes}</p>
                             </div>
-                            <p id="comment-votes">votes: {comment.votes}</p>
-                        </div>
-                    </li>
+                            { currentUser === comment.author ? <DeleteIcon onClick={() => {handleDelete(comment)}} id="deleteComment-icon"></DeleteIcon> : null}
+                        </li>
+                    )
                 })}
             </ul>
         </>
